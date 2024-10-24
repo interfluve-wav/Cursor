943719821b02ae6f717bbda92de8ff15a7b7f850
interfluve-wav <125309833+interfluve-wav@users.noreply.github.com>
2024-10-24 19:34:06 -0400

    Update index.js

--- my-node-project/my-node-project/index.js
+++ my-node-project/my-node-project/index.js
@@ -1,2 +1,13 @@
-console.log("Hello,Bitches!");
-console.log('Hello, World!');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', (name) => {
+  rl.question('What is your age? ', (age) => {
+    console.log(`Hello, ${name}! You are ${age} years old.`);
+    rl.close();
+  });
+});
